a553769d7fe0be28f66b74392623f1397bbb23f2
oumaima-elkachai <elkachaioumaima@gmail.com>
2025-10-10 17:24:36 +0100

    import missing

--- backend/stage-ete-main/Formation-Service/src/test/java/tn/esprit/spring/formationservice/FormationServiceApplicationTests.java
+++ backend/stage-ete-main/Formation-Service/src/test/java/tn/esprit/spring/formationservice/FormationServiceApplicationTests.java
@@ -2,6 +2,7 @@ package tn.esprit.spring.formationservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test") // Utilise application-test.properties
